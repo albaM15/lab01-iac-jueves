4445025a08ab10a9516d2a9a81118862636eaedb
alba <albamalca15@gmail.com>
2026-04-23 09:16:24 -0500

    feature: inicialización de infraestructura de terraform.

---
+++ iac/web.tf
@@ -0,0 +1,10 @@
+resource "docker_container" "web" {
+    name = "web-${terraform.workspace}"
+    image = "lab/web"
+
+    ports {
+        internal = "80"
+        external = var.web_port[terraform.workspace]
+    }
+}
+    
